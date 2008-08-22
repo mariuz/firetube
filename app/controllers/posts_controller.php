4fa282c76665687cc8a61a69f320d64b00b7b2e5
mariuz <mapopa@gmail.com>
2008-08-22 18:40:15 +0300

    added view action

--- app/controllers/posts_controller.php
+++ app/controllers/posts_controller.php
@@ -4,7 +4,12 @@ class PostsController extends AppController {
 	var $name = 'Posts';
 
 	function index() {
-		$this->set('posts', $this->Post->find('all'));
+		 $this->set('posts', $this->Post->find('all'));
+	}
+
+	function view($id = null) {
+		$this->Post->id = $id;
+		$this->set('post', $this->Post->read());
 	}
 }
 ?>
