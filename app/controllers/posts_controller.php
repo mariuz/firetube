30a27eb0b91ad9495f7400b78b15e1978448cf82
Marius Popa <mapopa@gmail.com>
2008-08-23 10:46:24 +0300

    http://book.cakephp.org/view/337/adding-posts

--- app/controllers/posts_controller.php
+++ app/controllers/posts_controller.php
@@ -1,15 +1,23 @@
 <?php
 class PostsController extends AppController {
-
 	var $name = 'Posts';
 
 	function index() {
-		 $this->set('posts', $this->Post->find('all'));
+		$this->set('posts', $this->Post->find('all'));
 	}
 
-	function view($id = null) {
+	function view($id) {
 		$this->Post->id = $id;
 		$this->set('post', $this->Post->read());
+
+	}
+
+	function add() {
+		if (!empty($this->data)) {
+			if ($this->Post->save($this->data)) {
+				$this->flash('Your post has been saved.', '/posts');
+			}
+		}
 	}
 }
 ?>
