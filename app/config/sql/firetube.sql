6660d15a042c7f2f69a14a938d49d512d96c7496
mariuz <mapopa@gmail.com>
2008-08-27 19:14:05 +0300

    added movies db http://mapopa.blogspot.com/2008/08/for-new-movies-page-i-have-created.html

--- app/config/sql/firetube.sql
+++ app/config/sql/firetube.sql
@@ -1,35 +1,54 @@
+/********************* ROLES **********************/
+
+/********************* UDFS ***********************/
+
 /****************** GENERATORS ********************/
 
+CREATE GENERATOR GEN_MOVIES_ID;
 CREATE GENERATOR GEN_POSTS_ID;
+/******************** DOMAINS *********************/
+
+/******************* PROCEDURES ******************/
 
 /******************** TABLES **********************/
 
-CREATE TABLE POSTS
-
+CREATE TABLE MOVIES
 (
-
-ID Integer NOT NULL,
-
-TITLE Varchar(50),
-
-BODY Varchar(2000),
-
-CREATED Timestamp,
-
-MODIFIED Timestamp,
-
-PRIMARY KEY (ID)
-
+  ID Integer NOT NULL,
+  FILENAME Varchar(512) NOT NULL,
+  PRIMARY KEY (ID)
 );
+CREATE TABLE POSTS
+(
+  ID Integer NOT NULL,
+  TITLE Varchar(50),
+  BODY Varchar(2000),
+  CREATED Timestamp,
+  MODIFIED Timestamp,
+  PRIMARY KEY (ID)
+);
+/********************* VIEWS **********************/
+
+/******************* EXCEPTIONS *******************/
 
 /******************** TRIGGERS ********************/
 
 SET TERM ^ ;
-
-CREATE TRIGGER POSTS_BI FOR POSTS ACTIVE
-
+CREATE TRIGGER MOVIES_BI FOR MOVIES ACTIVE
 BEFORE INSERT POSITION 0
+AS
 
+BEGIN
+
+IF (NEW.ID IS NULL) THEN
+
+NEW.ID = GEN_ID(GEN_MOVIES_ID, 1);
+
+END^
+SET TERM ; ^
+SET TERM ^ ;
+CREATE TRIGGER POSTS_BI FOR POSTS ACTIVE
+BEFORE INSERT POSITION 0
 AS
 
 BEGIN
@@ -39,12 +58,16 @@ IF (NEW.ID IS NULL) THEN
 NEW.ID = GEN_ID(GEN_POSTS_ID, 1);
 
 END^
-
 SET TERM ; ^
 
 GRANT DELETE, INSERT, REFERENCES, SELECT, UPDATE
+ ON MOVIES TO SYSDBA WITH GRANT OPTION;
 
-ON POSTS TO SYSDBA WITH GRANT OPTION;
+GRANT DELETE, INSERT, REFERENCES, SELECT, UPDATE
+ ON POSTS TO SYSDBA WITH GRANT OPTION;
+
+
+
 INSERT INTO "POSTS" ("TITLE","BODY","MODIFIED","CREATED") VALUES ('Foo bazz bar','test blog ','now','now');
 
 INSERT INTO "POSTS" ("TITLE","BODY","MODIFIED","CREATED") VALUES (' bazz bar Foo','test blog2 ','now','now');
