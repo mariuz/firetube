fe1366d27b1e988f913e924869331c5714201416
mariuz <mapopa@gmail.com>
2008-08-28 19:08:12 +0300

    added movies controller

---
+++ app/controllers/movies_controller.php
@@ -0,0 +1,27 @@
+<?php
+class MoviesController extends AppController {
+	var $name = 'Movies';
+
+	function index() {
+		$this->set('movies', $this->Movie->find('all'));
+	}
+
+	function view($id) {
+		$this->Movie->id = $id;
+		$this->set('movie', $this->Post->read());
+
+	}
+
+	function add() {
+		if (!empty($this->data)) {
+			if ($this->Movie->save($this->data)) {
+				$this->flash('Your Movie has been saved.', '/movies');
+			}
+		}
+	}
+	function delete($id) {
+        $this->Movie->del($id);
+        $this->flash('The Movie with id: '.$id.' has been deleted.', '/movies');
+	}
+}
+?>
