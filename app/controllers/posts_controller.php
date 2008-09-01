0b8ff6a5d11430aa8e334120d0678837952f4e42
mariuz <mapopa@gmail.com>
2008-09-01 17:16:53 +0300

    posts php5 cleanup http://groups.google.com/group/cake-php/browse_thread/thread/c543d6648c5c6e6f#

--- app/controllers/posts_controller.php
+++ app/controllers/posts_controller.php
@@ -1,7 +1,5 @@
 <?php
 class PostsController extends AppController {
-	var $name = 'Posts';
-
 	function index() {
 		$this->set('posts', $this->Post->find('all'));
 	}
--- app/models/post.php
+++ app/models/post.php
@@ -1,8 +1,6 @@
 <?php
 class Post extends AppModel
 {
-	var $name = 'Post';
-
 	var $validate = array(
 		'title' => array(
 			'rule' => array('minLength', 1)
