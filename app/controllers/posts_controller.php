5093b2e1c05f59c7c6a006c1b7d4491b9e8f91d3
Marius Popa <mapopa@gmail.com>
2008-09-04 00:19:04 +0300

    imported simple dom in controller
    http://simplehtmldom.sourceforge.net/manual.htm

--- app/controllers/posts_controller.php
+++ app/controllers/posts_controller.php
@@ -1,4 +1,6 @@
 <?php
+App::import('Vendor', 'simplehtmldom'.DS.'simple_html_dom');
+
 class PostsController extends AppController {
 	function index() {
 		$this->set('posts', $this->Post->find('all'));
